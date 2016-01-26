33f736c749e442b42fa1760b2fe68fa21e77090d
JannieBunny <christiaan.rossouw0@gmail.com>
2016-01-26 16:33:16 +0200

    Basic RSS Feed Link grabber

--- raspy.js
+++ raspy.js
@@ -1,57 +1,103 @@
-var slackAPI = require('slackbotapi');
+//Import
+var SlackAPI = require('slackbotapi');
 var fs = require('fs');
 var https = require('https');
+var http = require('https');
+var _ = require('lodash');
 
-var slack = new slackAPI({
-    'token': 'Token from Slack',
+//Setup
+console.log('Init Slack API');
+var slack = new SlackAPI({
+    'token': 'Slack API Token',
     'logging': true,
     'autoReconnect': true
+});
+
+var config;
+
+//Read Conf
+fs.readFile('conf/config.json', 'utf8', function(error, data){
+	if(error){
+		console.log(error);
+		return;	
+	}
+	console.log('Reading Config');
+	config = JSON.parse(data);
+	console.log(config);	
 });
 
 slack.on('message', function (data) {
     if (typeof data.text == 'undefined') 
 		return;
 	
-	if(slack.getChannel(data.channel).name !== "general"){
-		fs.appendFile('message.txt', data.text + '\n', function (err) {
-
-		});	
-	}
-});
-
-(function(){
-    setTimeout(function(){
-		fs.readFile('regexpattern.txt', function read(err, regex) {
-			if (err)
-				throw err;
-			processRSS(new RegExp(regex.toString()));
-		});
-	}, 5000);
-})();
-
-var processRSS = function(regex){
-	console.log(regex);
-	var lineReader = require('readline').createInterface({
-	  input: require('fs').createReadStream('message.txt')
+	var links = _.filter(config, function(item){
+		return item.type === "links";
 	});
-	lineReader.on('line', function (line) {
-		var url = regex.exec(line);
-		if(url && url.length > 0){
-			var urlDownload = url[1];	
-			var file = fs.createWriteStream(generateUUID() + '.torrent');
-			var request = https.get(urlDownload, function(response) {
-			  response.pipe(file);
-			});
+	console.log(links);
+	_.forEach(links, function(item){
+		if(slack.getChannel(data.channel).name === item.channel){
+			processLinks(data.text, item.keywords, item.regex_url, item.regex_name, item.download_dir);
 		}
 	});
+});
+
+function processLinks(text, keywords, regexURL, regexName, downloadDir) {
+	var linksToProcess = text.split('\n');
+	_.forEach(linksToProcess, function(link){
+		console.log('Message Link: ' + link);
+		var file_name = new RegExp(regexName).exec(text);
+		if(file_name && file_name.length > 0){
+			var filename = file_name[1];
+			console.log('File Name: ' + filename);
+			_.forEach(keywords, function(keyword){
+				console.log('Checking Keyword: ' + keyword.name);
+				if(filename.toLowerCase().indexOf(keyword.name.toLowerCase()) > 1){
+					console.log('Yay we found something');
+					var file = fs.createWriteStream(filename);
+					if(keyword.isTVContent){
+						var season_episode = new RegExp("([sS][0-9]{2}[eE][0-9]{2})").exec(filename);
+						if(season_episode && season_episode.length > 0){
+							var seriesPosition = season_episode[1];
+							console.log('Episode Number: ' + seriesPosition);
+							file = fs.createWriteStream(filename + ' ' + seriesPosition);
+							fs.readdir(downloadDir , function(error, files){
+								var duplicates = _.filter(files, function(item){
+									return item.indexOf(seriesPosition) > 0 && item.indexOf(filename.toLowerCase()) > 0; 
+								});
+								if(typeof duplicates !== 'undefined' && duplicates.length > 0){
+									//Duplicate Skip
+									console.log('Duplcate Detected, ignoring');
+								}
+								else{
+									console.log('File Name: ' + file);
+									downloadFile(text, regexURL, file);
+								}
+							});
+						}
+					}
+					else{
+						console.log('File Name: ' + file);
+						downloadFile(text, regexURL, file);
+					}
+				}
+			});
+		}		
+	});
 };
 
-function generateUUID() {
-    var d = new Date().getTime();
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = (d + Math.random()*16)%16 | 0;
-        d = Math.floor(d/16);
-        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
-    });
-    return uuid;
-};
+function downloadFile(text, regexURL, file) {
+	var url = new RegExp(regexURL).exec(text);
+	if(url && url.length > 0){
+		var urlDownload = url[1];
+		if (urlDownload.indexOf("http://") < 1){
+			http.get(urlDownload, function(response) {
+				response.pipe(file);
+			});
+		}
+		else{
+			https.get(urlDownload, function(response) {
+				response.pipe(file);
+			});
+		}
+	}	
+}
